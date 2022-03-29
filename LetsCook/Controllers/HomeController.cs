3a779e3716f048a40e733d8c3fb3e94a8e21fa2a
Ivana Angelovska <ivana.angelovska89@gmail.com>
2022-03-29 16:23:58 +0200

    Add project files.

---
+++ LetsCook/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿
+namespace LetsCook.Controllers
+{
+    using System.Diagnostics;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using LetsCook.Models;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Privacy() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
